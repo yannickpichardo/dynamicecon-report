9c5816f7d227878bad8c7301b1885bb3f17c56ab
Yannick Pichardo <yannickpichardo@gmail.com>
2020-04-29 11:51:43 +0200

    yannick test file

---
+++ WEItest_Yannick.R
@@ -0,0 +1,8 @@
+library('readxl')  # To read Excel files
+library('fpp2')    # For forecasting
+library('tseries') # To estimate ARMA models
+library('dynlm')   # To estimate ARDL models
+library('urca')    # For the Dickey Fuller test
+library('corrplot')# For plotting correlation matrices
+library('quadprog')# For quadratic optimization
+library('forecast')
