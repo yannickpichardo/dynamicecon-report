33157ed6aca9b1548c207add32a4b067efc41038
SamAnsari <60716778+SamAnsari@users.noreply.github.com>
2020-05-07 14:16:52 +0200

    topcode

--- CodevanSam.R
+++ CodevanSam.R
@@ -25,6 +25,19 @@ BBchange <- PercChange(data = data, Var = "BB", NewVar = "BBchange")
 BBchange <- BBchange$BBchange
 data$BBchange <- BBchange
 
+M1change <- PercChange(data = data, Var = "M1", NewVar = "M1change")
+M1change <- M1change$M1change
+data$M1change <- M1change
+
+WEIchange <- PercChange(data = data, Var = "WEI", NewVar = 'WEIchange')
+WEIchange <- WEIchange$WEIchange
+data$WEIchange <- WEIchange
+
+
+SP500change <- PercChange(data = data, Var = "S&P500", NewVar = "SP500change")
+SP500change <- SP500change$SP500change
+data$SP500change <- SP500change
+
 data$lnSP500 <- log(data$`S&P500`)
 data$lnBB <- log(data$BB)
 data$lnM1 <- log(data$M1)
@@ -34,16 +47,19 @@ data$lnOil <- log(data$Oil)
 
 plot1 <- ggplot(data = data, mapping = aes(x = BB, y = WEI)) +
             geom_point()
+plot1
 
 plot2 <- ggplot(data = data, mapping = aes(x = T10Y3M, y = wEI)) +
             geom_point() 
+plot2
 
 plot3 <- ggplot(data = data, mapping = aes(x = M1, y = WEI)) +
             geom_point()
+plot3
 
 plot4 <- ggplot(data = data) +
   geom_line(mapping = aes(x = Date, y = WEI), color = "blue") +
-  geom_line(mapping = aes(x = Date, y = T10Y3M, color = "red"))
+  geom_line(mapping = aes(x = Date, y = T10Y30M, color = "red"))
 plot4
 
 plot5 <- ggplot(data = data) +
@@ -60,8 +76,42 @@ plot6 <- ggplot(data = data) +
 plot6
 
 plot7 <- ggplot(data = data) + 
-  geom_point(mapping = aes(x = Date, y = WEI, color = "red")) +
-  geom_point(mapping = aes(x = Date, y = ))
+  geom_line(mapping = aes(x = Date, y = WEIchange, color = "red")) +
+  geom_line(mapping = aes(x = Date, y = M1change, color = "blue"))
+plot7
+
+plot8 <- ggplot(data = data) +
+  geom_line(aes(x = Date, y = WEI, color = "Red")) +
+  geom_line(aes(x = Date, y = BBchange, color = "Blue"))
+plot8
+
+plot9 <- ggplot(data = data) +
+  geom_line(aes(x = Date, y = BBchange, color = "Red")) + 
+  geom_line(aes(x = Date, y = M1change, color = "Blue"))
+plot9
+
+plot10 <- ggplot(data = data) +
+  geom_line(aes(x = Date, y = WEI, color = "Red")) + 
+  geom_line(aes(x = Date, y = SP500change, color = "Blue"))
+plot10
+
+plot11 <- ggplot(data = data) + 
+  geom_line(aes(x = Date, y = Oil*10, color = "red")) + 
+  geom_line(aes(x = Date, y = WEI*100, color = "blue"))
+plot11
+
+plot12 <- ggplot(data = data) +
+  geom_line(aes(x = Date, y = WEI*1000, color = "red")) +
+  geom_line(aes(x = Date, y = `S&P500`*10, color = "blue"))
+plot12
+
+plot13 <- ggplot(data = data) + 
+  geom_line(mapping = aes(x = Date, y = WEIchange, color = "red"))
+plot13
+
+data[(which.min(data$WEIchange) - 1):(which.min(data$WEIchange) + 1), ]
+plot(x = data$Date, y = data$WEI, type = "l")
+data(length(data$WEIchange - 10))
 
 par(mfcol = c(2,2))
 plot(data$BB ,data$WEI)
@@ -75,6 +125,8 @@ print(x)
 cor(data[4:10])
 corrplot(data[4:10], type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
 cor(data[4:11])
+cor(data[-1])
+cor.test(x = data$WEI, y = data$`S&P500`, method=c("pearson", "kendall", "spearman"))
 
 
 ## Regressing several variables to identify statsitical significance ##
@@ -96,8 +148,10 @@ model4 <- lm(WEI ~ lnSP500 + Oil + FFR, data = data)
 summary(model4)
 plot(model4)
 
-model5 <- lm.(WEI ~ lnSP500 + lnBB + lnM1 + lnOil, data = data)
+model5 <- lm(WEI ~ lnSP500 + lnBB + lnM1 + lnOil, data = data)
 summary(model5)
 plot(model5)
 
+model5 <- lm(WEI ~ lnOil + FFR + T10Y3M, data = data)
+summary(model5)
 
