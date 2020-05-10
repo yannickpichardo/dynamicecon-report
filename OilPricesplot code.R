e44fce09420185fdabe43da7341473468e6096b3
Yannick Pichardo <yannickpichardo@gmail.com>
2020-05-10 15:10:12 +0200

    Add code for time series of oil prices

---
+++ OilPricesplot code.R
@@ -0,0 +1,34 @@
+##Libraries
+install.packages('ggfortify')
+library(ggfortify)
+
+## Data input
+data = read.xlsx("WEI.xlsx", sheet = 2, detectDates = T)
+oilp = data %>% select(6)
+## 
+
+
+
+##Wei change
+WEI_time_series = ts(data[,4], start= c(2008), frequency = 365.25/7)
+WEI_ts_dataframe = data.frame(WEI_time_series)
+
+
+
+oil_time_series = ts(data[,6], start = c(2008), frequency = 365.25/7)
+
+oil_time_series_change = diff(oil_time_series)
+
+autoplot(oil_time_series_change)
+
+
+data = data %>% cbind(oil_time_series_change)
+
+time_series = ts(cbind(data[,4],data[,6]), start = c(2008), frequency = 365.25/7)
+autoplot(time_series)
+
+ 
+
+##WEI plot 
+
+
