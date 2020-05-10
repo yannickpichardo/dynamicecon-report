02bc0c668b344b54309a60c467f6b5e0da5d7614
Yannick Pichardo <yannickpichardo@gmail.com>
2020-05-10 15:38:55 +0200

    Add differences and plot these with the correct vectors

--- OilPricesplot code.R
+++ OilPricesplot code.R
@@ -22,13 +22,44 @@ oil_time_series_change = diff(oil_time_series)
 autoplot(oil_time_series_change)
 
 
-data = data %>% cbind(oil_time_series_change)
-
 time_series = ts(cbind(data[,4],data[,6]), start = c(2008), frequency = 365.25/7)
 autoplot(time_series)
 
- 
 
-##WEI plot 
+
+##Differences
+
+data = PercChange(data = data, Var = "Oil", NewVar = "Oilchange")
+
+
+WEI_time_series_change <- diff(data$WEI)
+WEI_time_series_change <- append(WEI_time_series_change, 0, after = 0)
+data <- data %>% cbind(WEI_time_series_change)
+
+
+wei_percchange = PercChange(data = data, Var = "WEI", NewVar = "WEIchange")
+
+
+
+
+
+##plots
+
+wei_vs_changeoil <- ggplot(data = data) + 
+  geom_line(aes(x = Date, y = WEI, color = "darkred")) + 
+  geom_line(aes(x = Date, y = Oilchange, color = "lightblue")) + 
+  geom_line(aes(x = Date, y = 0 , color = "green")) +
+  ggtitle("WEI vs Change in Oil Price")
+wei_vs_changeoil
+
+
+changewei_vs_changeoil <- ggplot(data = data) + 
+  geom_line(aes(x = Date, y = WEI_time_series_change, color = "darkred")) + 
+  geom_line(aes(x = Date, y = Oilchange, color = "lightblue")) + 
+  geom_line(aes(x = Date, y = 0 , color = "green")) +
+  ggtitle("WEI vs Change in Oil Price")
+changewei_vs_changeoil
+
+
 
 
